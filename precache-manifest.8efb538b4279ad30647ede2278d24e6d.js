86791355dfa27013b84811ea8ffdfd55827bf643
yo-onhye <woony0807@gmail.com>
2020-08-17 00:15:52 +0900

    Updates

---
+++ precache-manifest.8efb538b4279ad30647ede2278d24e6d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32ff31f252895d6ff794d0f2d88e1052",
+    "url": "/06.project-collect-sns/index.html"
+  },
+  {
+    "revision": "e3ee097b34898f9ac74e",
+    "url": "/06.project-collect-sns/static/css/main.b0d27263.chunk.css"
+  },
+  {
+    "revision": "deaca9942ef7a8fa3632",
+    "url": "/06.project-collect-sns/static/js/2.86eb2637.chunk.js"
+  },
+  {
+    "revision": "c607541aec0fb4887a90f60037cb0bdf",
+    "url": "/06.project-collect-sns/static/js/2.86eb2637.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3ee097b34898f9ac74e",
+    "url": "/06.project-collect-sns/static/js/main.b17f7acb.chunk.js"
+  },
+  {
+    "revision": "ad5c440867b7270af95f",
+    "url": "/06.project-collect-sns/static/js/runtime-main.bc0ebfaf.js"
+  }
+]);
